9854d498d42029aea441ae8f97a45e5bfb5f8673
tanmay <tan2018carlson@gmail.com>
2025-09-14 16:15:54 +0530

    trivy scan file updated

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -1,24 +1,31 @@
 // vars/run_tests.groovy
-def call(Map config = [:]) {
-    // Default test command if not provided
-    // def testCommand = config.testCommand ?: "npm test"
+
+def call() {
+    echo "Running unit tests..."
     
-    // def nodeTool = config.nodeTool ?: "Node18"
-
-    // echo "Running unit tests with Node tool: ${nodeTool}"
-
-    // echo "Running unit tests with command: ${testCommand}"
-
-    // // Execute the test command
-    // // sh """
-    // //     ${testCommand}
-    // // """
-    // withEnv(["PATH+NODE=${tool nodeTool}/bin"]) {
-    //     sh """
-    //         npm ci
-    //         ${testCommand}
-    //     """
-        echo "Unit tests completed successfully"
-
-    }
+    // Placeholder - no tests defined
+    echo "Unit tests completed successfully"
 }
+
+// def call(Map config = [:]) {
+//     // Default test command if not provided
+//     // def testCommand = config.testCommand ?: "npm test"
+    
+//     // def nodeTool = config.nodeTool ?: "Node18"
+
+//     // echo "Running unit tests with Node tool: ${nodeTool}"
+
+//     // echo "Running unit tests with command: ${testCommand}"
+
+//     // // Execute the test command
+//     // // sh """
+//     // //     ${testCommand}
+//     // // """
+//     // withEnv(["PATH+NODE=${tool nodeTool}/bin"]) {
+//     //     sh """
+//     //         npm ci
+//     //         ${testCommand}
+//     //     """
+
+//     }
+// }
