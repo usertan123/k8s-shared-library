5a29c74a13bb99ac56fe1f397d5d2f5b1a88303f
tanmay <tan2018carlson@gmail.com>
2025-09-14 15:26:29 +0530

    trivy scan file updated

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -2,11 +2,21 @@
 def call(Map config = [:]) {
     // Default test command if not provided
     def testCommand = config.testCommand ?: "npm test"
+    
+    def nodeTool = config.nodeTool ?: "NodeJS 18.0.0"
+
+    echo "Running unit tests with Node tool: ${nodeTool}"
 
     echo "Running unit tests with command: ${testCommand}"
 
     // Execute the test command
-    sh """
-        ${testCommand}
-    """
+    // sh """
+    //     ${testCommand}
+    // """
+    withEnv(["PATH+NODE=${tool nodeTool}/bin"]) {
+        sh """
+            npm install
+            ${testCommand}
+        """
+    }
 }
